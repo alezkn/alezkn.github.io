4e7062cbae6fec91a326f5051a8d90b4d06b6768
Alessandro Costa <alessandrocosta321@hotmail.com>
2022-05-10 15:46:41 -0300

    New headers, add an easter egg to the console

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
+console.log("Hello dear inspector!");
+
 function playAudio() {
   var audio = new Audio(
     `src/assets/sound/grub_0${Math.floor(Math.random() * (6 - 1)) + 1}.wav`
   );
-  console.log(audio);
   audio.volume = 0.1;
   audio.play();
 }
